8fadf598a813c36253b0ee4f827ef0b557eb1e4e
kaito <kaito@mfac.jp>
2021-10-02 01:48:41 +0900

    fix: fix type LocalDispatch is exported

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { defineModule } from "./define-module"
 import type { TypedStore } from "./typed-store"
 import type { MapState, MapGetters, MapActions } from "./utility"
+import type { LocalDispatch } from "./core"
 
 export { defineModule }
-export type { TypedStore, MapState, MapGetters, MapActions }
+export type { TypedStore, LocalDispatch, MapState, MapGetters, MapActions }
